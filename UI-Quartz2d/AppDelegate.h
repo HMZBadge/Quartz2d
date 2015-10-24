93fbbc2160a4f0d6b051d33d342d40e067fb21d7
zhaozhidan <zhidan0213@icloud.com>
2015-10-24 09:26:05 +0800

    Initial Commit

---
+++ UI-Quartz2d/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UI-Quartz2d
+//
+//  Created by 赵志丹 on 15/10/24.
+//  Copyright © 2015年 赵志丹. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
